04a1c533e478d3290c5b08efc4345494009c15d0
SKE <appybuildmore@gmail.com>
2017-07-23 21:27:14 -0500

    First Commit

---
+++ Strings and String Interpolation.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+var str = "Hello, playground"
+
+var name: String = "SKE"
+var verb: String = "loves"
+var noun = "Romni"
+
+let hisFacebookPage = "Duc Tran - Learn. Create. Contribute"
+let numberOfLikes = 100000
+
+let someText = "\(hisFacebookPage) has \(numberOfLikes) likes!"
+print(someText)
+*/
+
+var name = "SKE"
+var verb = "grinds"
+var adverb = "constantly"
+var noun = "Swift"
+var adjective = "beautiful"
+
+var aStory = "\(name) \(adverb) \(verb) daily in an effort to learn \(noun) a \(adjective) programming language!"
+
+print(aStory)
